ccf9612285aa467a1a46b4b48a78d355a97a818b
diegoasp10 <diego.saavedra@nuvu.cc>
2025-03-13 18:39:41 -0500

    Initial commit

---
+++ lib/aws-quicksight-s3-file-downloader-stack.ts
@@ -0,0 +1,16 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class AwsQuicksightS3FileDownloaderStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'AwsQuicksightS3FileDownloaderQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+  }
+}
---
+++ test/aws-quicksight-s3-file-downloader.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsQuicksightS3FileDownloader from '../lib/aws-quicksight-s3-file-downloader-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-quicksight-s3-file-downloader-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsQuicksightS3FileDownloader.AwsQuicksightS3FileDownloaderStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
